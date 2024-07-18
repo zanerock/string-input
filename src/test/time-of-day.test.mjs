5d4257b88a5e53f1affc65e1174558ae979d5c90
Zane Rockenbaugh <zane@liquid-labs.com>
2024-07-18 11:12:34 -0500

    implemented and tested time of day validations

--- src/test/time-of-day.test.mjs
+++ src/test/time-of-day.test.mjs
@@ -11,7 +11,13 @@ describe('TimeOfDay', () => {
     ['09:10:30 PM', undefined, 21, 10, 30, 0],
     ['9:10:30.509 PM', undefined, 21, 10, 30, 0.509],
     ['9:10 AM', undefined, 9, 10, 0, 0],
-    ['12:30 PM', undefined, 0, 30, 0, 0]
+    ['12:30 PM', undefined, 0, 30, 0, 0],
+    ['12:00', { max: '12:01' }, 12, 0, 0, 0],
+    ['12:00', { max: '12:00' }, 12, 0, 0, 0],
+    ['12:00', { min: '11:59' }, 12, 0, 0, 0],
+    ['12:00', { min: '12:00' }, 12, 0, 0, 0],
+    ['12:00', { validateInput : ({ input }) => /:/.test(input) }, 12, 0, 0, 0],
+    ['12:00', { validateValue : ({ value }) => value.getSeconds() === 0 }, 12, 0, 0, 0]
   ]
 
   const invalidInput = [
@@ -19,8 +25,12 @@ describe('TimeOfDay', () => {
     [null, {}, 'is null or undefined'],
     [12, {}, 'must be a string'],
     ['foo', {}, 'not recognized'],
-    ['2400', { noEOD : true }, 'disallowed special']
-  ]
+    ['2400', { noEOD : true }, 'disallowed special'],
+    ['12:00', { max: '11:59' }, "must be less than or equal to '11:59:00'"],
+    ['12:00', { min: '12:01' }, "must be greater than or equal to '12:01:00"],
+    ['1200', { validateInput : ({ input }) => /:/.test(input) }, 'failed custom input validation'],
+    ['12:00', { validateValue : ({ value }) => value.getSeconds() !== 0 }, 'failed custom value validation']
+  ].map((params) => { params[1].name = 'foo'; params[2] = "Time of day 'foo'.*?" + params[2]; return params })
 
   test.each(validInput)('%s, options %p => hours: %s, minutes: %s, seconds: %s, frac seconds: %s', 
     (input, options, hours, mins, secs, fracSecs) => {
--- src/time-of-day.mjs
+++ src/time-of-day.mjs
@@ -1,5 +1,8 @@
 import { militaryTimeRE, timeRE, twentyFourHourTimeRE } from 'regex-repo'
 
+import { checkMaxMin } from './lib/check-max-min'
+import { checkValidateInput } from './lib/check-validate-input'
+import { checkValidateValue } from './lib/check-validate-value'
 import { describeInput } from './lib/describe-input'
 import { typeChecks } from './lib/type-checks'
 
@@ -22,14 +25,41 @@ const TimeOfDay = function (
   const twentyFourHourTimeMatch = input.match(twentyFourHourTimeRE)
 
   if (militaryTimeMatch === null && timeMatch === null && twentyFourHourTimeMatch === null) {
-    throw Error(`selfDescription value '${input}' not recognized as either military, standard, or 24-hour time. Try something like '2130', 9:30 PM', or '21:30'.`)
+    throw Error(`${selfDescription} value '${input}' not recognized as either military, standard, or 24-hour time. Try something like '2130', 9:30 PM', or '21:30'.`)
   }
 
   const isEOD = militaryTimeMatch?.[1] !== undefined || twentyFourHourTimeMatch?.[1] !== undefined
   if (noEOD === true) {
-    throw new Error(`selfDescription indicates disallowed special 'end-of-day' time.`)
+    throw new Error(`${selfDescription} indicates disallowed special 'end-of-day' time.`)
   }
 
+  checkValidateInput({ input, selfDescription, validateInput })
+
+  const value = getValue({ isEOD, militaryTimeMatch, timeMatch, twentyFourHourTimeMatch })
+  
+  if (max !== undefined) {
+    max = TimeOfDay(max, { name: `${name}' constraint 'max` })
+  }
+  if (min !== undefined) {
+    min = TimeOfDay(min, { name: `${name}' constraint 'max` })
+  }
+  checkMaxMin({ input, limitToString : limitDescriptor, max, min, selfDescription, value })
+
+  checkValidateValue({ input, value, selfDescription, validateValue })
+
+  return value
+}
+
+const limitDescriptor = (limit) =>{
+  const hours = limit.getHours()
+  const minutes = limit.getMinutes()
+  const seconds = limit.getSeconds()
+  const fracSeconds = limit.getFractionalSeconds()
+
+  return `${hours}:${('' + minutes).padStart(2, '0')}:${('' + seconds).padStart(2, '0')}${('' + fracSeconds).slice(1)}`
+}
+
+const getValue = ({ isEOD, militaryTimeMatch, timeMatch, twentyFourHourTimeMatch }) => {
   let hours, minutes, seconds, fracSeconds
   if (isEOD === true) {
     hours = 24
@@ -56,7 +86,8 @@ const TimeOfDay = function (
     getHours             : () => hours,
     getMinutes           : () => minutes,
     getSeconds           : () => seconds,
-    getFractionalSeconds : () => fracSeconds
+    getFractionalSeconds : () => fracSeconds,
+    valueOf              : () => (hours * 60 * 60) + (minutes * 60) + seconds + fracSeconds
   }
 }
 
